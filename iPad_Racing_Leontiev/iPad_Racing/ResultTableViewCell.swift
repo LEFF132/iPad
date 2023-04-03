15aba22f9ad1c7f5b1345f26368ccaa52444c6cc
LEFF132 <103582704+LEFF132@users.noreply.github.com>
2023-04-03 12:09:52 +0300

    Add files via upload

---
+++ iPad_Racing_Leontiev/iPad_Racing/ResultTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ResultTableViewCell.swift
+//  Drive
+//
+//  Created by Leontiev Timofey on 16/3/2023.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var namePlayer: UILabel!
+    @IBOutlet weak var gameResult: UILabel!
+    @IBOutlet weak var timeResult: UILabel!
+    
+   
+}
